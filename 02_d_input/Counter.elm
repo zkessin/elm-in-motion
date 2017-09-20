ad9b5b11ef2e2058b20532afc52677697df7b7ec
Zachary Kessin <zkessin@gmail.com>
2017-09-20 09:35:28 +0300

    add field

---
+++ 02_d_input/Counter.elm
@@ -0,0 +1,42 @@
+module Counter exposing (..)
+
+import Html exposing (Html, br, div, h1, input, label, text)
+import Html.Attributes as Attributes
+import Html.Events as Events
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Change Int
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text <| toString model ]
+        , div [ Events.onClick <| Change 1 ] [ text "+" ]
+        , div [ Events.onClick <| Change -1 ] [ text "-" ]
+        , div [ Events.onClick <| Change 3 ] [ text "+3" ]
+        , label [] [ text "Set A value" ]
+        , text " "
+        , input [ Attributes.placeholder "Set a Value" ] []
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change n ->
+            model + n
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = 0
+        , view = view
+        , update = update
+        }
